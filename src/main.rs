504569f0e10842388623c39d58458dd8020b9dec
OpenPendulum <arasam1067@gmail.com>
2024-08-04 18:22:48 -0400

    added runner submodule and pipeline tests

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
 use eyre::Result;
-use log_parser::log_processor::LogProcessor;
-use rayon::{prelude::*, ThreadPoolBuilder};
-use std::{
-    env,
-    fs::File,
-    io::{BufRead, BufReader},
-    path::Path,
-    sync::Arc,
-};
+use log_parser::runner::Runner;
+use std::{env, io::stdout};
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -16,35 +9,17 @@ fn main() -> Result<()> {
         return Ok(());
     }
 
-    // Configure the number of threads in the global rayon thread pool
-    ThreadPoolBuilder::new().num_threads(4).build_global()?; // Adjust the number of threads as needed
+    let log_file = &args[1];
 
-    let path = Path::new(&args[1]);
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
+    let stdout_writer = stdout();
 
-    let processor = Arc::new(LogProcessor::new()?);
+    let mut runner = Runner::builder()
+        .with_log_file(log_file)
+        .with_threads(4)
+        .with_stdout_writer(stdout_writer)
+        .build()?;
 
-    reader.lines().par_bridge().for_each(|line| {
-        if let Ok(line) = line {
-            let processor = Arc::clone(&processor);
-            processor.process_line(&line).unwrap_or_else(|err| {
-                eprintln!("Error processing line: {}", err);
-            });
-        }
-    });
-
-    // Capture the last pipeline if it was still in progress
-    {
-        let mut pipelines = processor.pipelines.lock().unwrap();
-        let mut current_pipeline = processor.current_pipeline.lock().unwrap();
-        if let Some(pipeline) = current_pipeline.take() {
-            pipelines.push(pipeline);
-        }
-    }
-
-    // Print the summary of each pipeline and the total aggregate duration
-    processor.print_summary();
+    runner.run()?;
 
     Ok(())
 }
